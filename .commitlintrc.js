c832523887012d5fabfd16e20a382f6847827d92
tienluu1301 <jordanluu1301@gmail.com>
2024-06-17 01:14:42 -0700

    chore: testing

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -21,7 +21,6 @@ module.exports = {
       ],
       'type-case': [2, 'always', 'lower-case'],
       'type-empty': [2, 'never'],
-      'scope-empty': [2, 'never'],
       'subject-empty': [2, 'never'],
       'subject-full-stop': [2, 'never', '.'],
       'header-max-length': [2, 'always', 72],
